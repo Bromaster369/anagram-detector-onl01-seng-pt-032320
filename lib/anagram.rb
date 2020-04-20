ea37cca5259947377259bbf3e7c17bb731e517fb
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-20 23:12:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+    
+  def match(array_of_words)
+  array = []
+  array_of_words.each do |word|
+    if word.split("").sort == @word.split("").sort
+    end 
+  end
+end
