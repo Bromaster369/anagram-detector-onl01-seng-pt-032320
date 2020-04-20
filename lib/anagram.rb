6f021abf44917f70a75f1dcd4e301976ae8941c0
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-20 23:12:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,11 +5,12 @@ class Anagram
   def initialize(word)
     @word = word
   end
-    
+  
   def match(array_of_words)
-  array = []
-  array_of_words.each do |word|
-    if word.split("").sort == @word.split("").sort
-    end 
+      array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
   end
+  
+  
 end
